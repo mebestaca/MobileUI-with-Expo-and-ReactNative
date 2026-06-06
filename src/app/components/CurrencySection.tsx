05094072cca0349deceedf408e9b5dc750da6f0f
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-05 22:02:57 -0600

    removed index header

---
+++ src/app/components/CurrencySection.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function CurrencySection() {
+  return (
+    <View>
+      <Text>Check</Text>
+    </View>
+  );
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,12 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Check</Text>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
