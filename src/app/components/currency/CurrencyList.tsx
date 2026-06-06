c508622cd187fd98cd1ffe661234a80a30271b79
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-06 16:13:56 -0600

    added currency layout

--- src/app/components/currency/CurrencyList.tsx
+++ src/app/components/currency/CurrencyList.tsx
@@ -1,13 +1,20 @@
 import { currencies } from "@/data/currencies";
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import CurrencyCard from "./CurrencyCard";
 
 export default function CurrencyList() {
   return (
-    <View>
+    <View style={styles.container}>
       {currencies.map((res) => (
         <CurrencyCard resource={res} key={res.id} />
       ))}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+  },
+});
