bb22c8195d8a12b2748d654284b0487ad7195dd8
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-05 22:53:58 -0600

    added lab section

---
+++ src/app/components/lab/LabSection.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function LabSection() {
+  return (
+    <View>
+      <Text>LAB</Text>
+      <View>
+        <Text>A</Text>
+        <Text>1.18M</Text>
+      </View>
+    </View>
+  );
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import CurrencySection from "./components/currency/CurrencySection";
+import LabSection from "./components/lab/LabSection";
 
 export default function Index() {
   return (
     <SafeAreaView>
       <CurrencySection />
+      <LabSection />
     </SafeAreaView>
   );
 }
