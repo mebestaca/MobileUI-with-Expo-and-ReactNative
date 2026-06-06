5b16825767cee3c9d579e58b86b03d0e0cbc8b69
Marc Edison Estaca <mebestaca@gmail.com>
2026-06-05 22:22:43 -0600

    added currency list

---
+++ src/app/components/CurrencyList.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function CurrencyList() {
+  return (
+    <View>
+      <Text>Currency 1</Text>
+      <Text>Currency 2</Text>
+      <Text>Currency 3</Text>
+    </View>
+  );
+}
--- src/app/components/CurrencySection.tsx
+++ src/app/components/CurrencySection.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import CurrencyList from "./CurrencyList";
 
 export default function CurrencySection() {
   return (
     <View>
-      <Text>Check</Text>
+      <CurrencyList />
     </View>
   );
 }
